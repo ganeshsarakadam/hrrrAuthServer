c2688faa45d0abc464416d2cb3c586d28d78aabb
ganesh-srv <ganeshsrv.sarakadam@gmail.com>
2023-11-29 04:52:50 -0500

    Added mounting, and create users endpoint

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_jwt_extended import JWTManager, create_access_token
 from flask_pymongo import PyMongo
+# from PyMongo import Mongo
 from datetime import timedelta
 from bson.json_util import dumps
 from bson.objectid import ObjectId
@@ -9,9 +10,10 @@ authServerApp = Flask(__name__)
 
 authServerApp.config['JWT_SECRET_KEY'] = 'hrrr-weather-lawn'  
 
+
 # TBD Change this
 # authServerApp.config['JWT_SECRET_KEY'] = 'hrrr-weather-lawn'  
-authServerApp.config["MONGO_URI"] = "mongodb://mongo:27017/lawnUsers"
+authServerApp.config["MONGO_URI"] = "mongodb://mongo:27017/lawn"
 # authServerApp.config["MONGO_URI"] = "mongodb://localhost:27017/lawnUsers"
 
 
@@ -22,13 +24,27 @@ mongo = PyMongo(authServerApp)
 def login():
     username = request.json.get('username', None)
     # print("username", username)
-    user = mongo.db.myCollection.find_one({"username": username})
+    user = mongo.db.lawn.find_one({"username": username})
     # print("User from MongoDB:", user)
     if user is None:
         return jsonify({"msg": "user not found"}), 401
 
     access_token = create_access_token(identity=username, expires_delta=timedelta(minutes=8))
     return access_token
+
+@authServerApp.route('/create/user', methods=['POST'])
+def create():
+    username = request.json.get('username', None)
+    details = request.json.get('details', {})
+    user = {"username":username}
+    for key, value in details.items():
+        user[f"details.{key}"] = value
+    mongo.db.lawn.update_one({"username": username}, {"$set": user}, upsert=True)
+    return jsonify({'msg':"user created or updated"}), 201
+
+
+
+
 
 if __name__ == '__main__':
     authServerApp.run(host="0.0.0.0", port=4200, debug=True)
